61d224e853607778edd94d48282bcaa27371fc5c
mkhanooo <mkhan2khan@live.com>
2022-03-30 19:36:38 +0500

    first commit

--- lib/chat_project_web/endpoint.ex
+++ lib/chat_project_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule ChatProjectWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
   socket "/socket", ChatProjectWeb.UserSocket,
-  websocket: true,
   websocket: [timeout: 45_000],
+  websocket: true,
   longpoll: false
   # Serve at "/" the static files from "priv/static" directory.
   #
